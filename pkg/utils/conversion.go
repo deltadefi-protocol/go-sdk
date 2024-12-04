3fb50f7eb99b88535d72f565bc1998b99d884e28
Ken Lau <kenlauhong2015@gmail.com>
2024-12-04 16:42:34 +0800

    .

---
+++ pkg/utils/conversion.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"strconv"
+
+	rmodels "github.com/sidan-lab/rum/models"
+)
+
+func ConvertUTxO(utxo *rmodels.UTxO) *InputUtxos {
+	return &InputUtxos{
+		TxHash:  utxo.Input.TxHash,
+		TxID:    strconv.Itoa(utxo.Input.OutputIndex),
+		Amount:  utxo.Output.Amount,
+		Address: utxo.Output.Address,
+	}
+}
+
+func ConvertUTxOs(utxos []*rmodels.UTxO) []*InputUtxos {
+	result := make([]*InputUtxos, len(utxos))
+	for i, utxo := range utxos {
+		result[i] = ConvertUTxO(utxo)
+	}
+	return result
+}
+
+func ConvertTxInParameter(txIn *TxInParameter) *InputUtxos {
+	return &InputUtxos{
+		TxHash:  txIn.TxHash,
+		TxID:    strconv.Itoa(txIn.TxIndex),
+		Amount:  txIn.Amount,
+		Address: txIn.Address,
+	}
+}
