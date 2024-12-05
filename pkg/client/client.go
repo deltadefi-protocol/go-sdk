abd35da0bb8dd16cf0740a53ec94c85f1fa67c08
Ken Lau <kenlauhong2015@gmail.com>
2024-12-05 15:36:25 +0800

    added accounts markets orders client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -18,20 +18,20 @@ type Client struct {
 	Jwt        string
 	SigningKey string
 	HTTPClient *http.Client
-	BaseUrl    string
+	BaseURL    string
 }
 
 func NewClient(apiKey string, network string, jwt string, signingKey string) *Client {
 	cfg := config.GetConfig()
 	var networkId uint8
-	var baseUrl string
+	var baseURL string
 
 	if network == "mainnet" {
 		networkId = uint8(1)
-		baseUrl = "https://api-dev.deltadefi.io" // TODO: input production link once available
+		baseURL = "https://api-dev.deltadefi.io" // TODO: input production link once available
 	} else {
 		networkId = uint8(0)
-		baseUrl = "https://api-dev.deltadefi.io"
+		baseURL = "https://api-dev.deltadefi.io"
 	}
 
 	return &Client{
@@ -42,7 +42,7 @@ func NewClient(apiKey string, network string, jwt string, signingKey string) *Cl
 		HTTPClient: &http.Client{
 			Timeout: time.Duration(cfg.Client.Timeout) * time.Minute,
 		},
-		BaseUrl: baseUrl,
+		BaseURL: baseURL,
 	}
 }
 
@@ -61,8 +61,8 @@ func (c *Client) sendRequest(req *http.Request, responseBody *string) error {
 		return fmt.Errorf("empty request")
 	}
 
-	req.Header.Set("Accept", "application/json")
-	req.Header.Add("api-key", c.apiKey)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("X-API-KEY", c.apiKey)
 	req.Header.Set("Authorization", c.Jwt)
 
 	if c.HTTPClient == nil {
@@ -100,7 +100,7 @@ func (c *Client) sendRequest(req *http.Request, responseBody *string) error {
 }
 
 func (c *Client) get(url string) (*http.Response, error) {
-	req, err := http.NewRequest("GET", c.BaseUrl+url, nil)
+	req, err := http.NewRequest("GET", c.BaseURL+url, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -108,8 +108,8 @@ func (c *Client) get(url string) (*http.Response, error) {
 		return nil, fmt.Errorf("empty request")
 	}
 
-	req.Header.Set("Accept", "application/json")
-	req.Header.Add("api-key", c.apiKey)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("X-API-KEY", c.apiKey)
 	req.Header.Set("Authorization", c.Jwt)
 
 	return c.HTTPClient.Do(req)
@@ -120,7 +120,7 @@ func (c *Client) post(url string, body interface{}) (*http.Response, error) {
 	if err != nil {
 		return nil, err
 	}
-	req, err := http.NewRequest("POST", c.BaseUrl+url, bytes.NewReader(jsonBody))
+	req, err := http.NewRequest("POST", c.BaseURL+url, bytes.NewBuffer(jsonBody))
 	if err != nil {
 		return nil, err
 	}
@@ -128,16 +128,19 @@ func (c *Client) post(url string, body interface{}) (*http.Response, error) {
 		return nil, fmt.Errorf("empty request")
 	}
 
-	req.Header.Set("Accept", "application/json")
-	req.Header.Add("api-key", c.apiKey)
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("X-API-KEY", c.apiKey)
 	req.Header.Set("Authorization", c.Jwt)
 
 	return c.HTTPClient.Do(req)
 }
 
-func (c *Client) postBuffer(url string, buffer []byte) (*http.Response, error) {
-	req, err := http.NewRequest("POST", c.BaseUrl+url, bytes.NewBuffer(buffer))
+func (c *Client) delete(url string, body interface{}) (*http.Response, error) {
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("DELETE", c.BaseURL+url, bytes.NewBuffer(jsonBody))
 	if err != nil {
 		return nil, err
 	}
@@ -145,10 +148,26 @@ func (c *Client) postBuffer(url string, buffer []byte) (*http.Response, error) {
 		return nil, fmt.Errorf("empty request")
 	}
 
-	req.Header.Set("Accept", "application/cbor")
-	req.Header.Add("api-key", c.apiKey)
-	req.Header.Set("Content-Type", "application/cbor")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("X-API-KEY", c.apiKey)
 	req.Header.Set("Authorization", c.Jwt)
 
 	return c.HTTPClient.Do(req)
 }
+
+// func (c *Client) postBuffer(url string, buffer []byte) (*http.Response, error) {
+// 	req, err := http.NewRequest("POST", c.BaseURL+url, bytes.NewBuffer(buffer))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	if req == nil {
+// 		return nil, fmt.Errorf("empty request")
+// 	}
+
+// 	req.Header.Set("Accept", "application/cbor")
+// 	req.Header.Add("X-API-KEY", c.apiKey)
+// 	req.Header.Set("Content-Type", "application/cbor")
+// 	req.Header.Set("Authorization", c.Jwt)
+
+// 	return c.HTTPClient.Do(req)
+// }
--- pkg/client/markets.go
+++ pkg/client/markets.go
@@ -1,1 +1,65 @@
 package client
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/deltadefi-protocol/go-sdk/pkg/api/requests"
+	"github.com/deltadefi-protocol/go-sdk/pkg/api/responses"
+)
+
+func (c *Client) getDepth(data *requests.GetMarketDepthRequest) (*responses.GetMarketDepthResponse, error) {
+	url := fmt.Sprintf("/market/depth?pair=%s", data.Pair)
+	resp, err := c.get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var getMarketDepthResponse responses.GetMarketDepthResponse
+	err = json.NewDecoder(resp.Body).Decode(&getMarketDepthResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &getMarketDepthResponse, nil
+}
+
+func (c *Client) getMarketPrice(data *requests.GetMarketPriceRequest) (*responses.GetMarketPriceResponse, error) {
+	url := fmt.Sprintf("/market/market-price?pair=%s", data.Pair)
+	resp, err := c.get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var getMarketPriceResponse responses.GetMarketPriceResponse
+	err = json.NewDecoder(resp.Body).Decode(&getMarketPriceResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &getMarketPriceResponse, nil
+}
+
+func (c *Client) getAggregatedPrice(data *requests.GetAggregatedPriceRequest) (*responses.GetAggregatedPriceResponse, error) {
+	var url string
+	if *data.Start == 0 && *data.End == 0 {
+		url = fmt.Sprintf("/market/aggregate/%s?interval=%s", data.Pair, data.Interval)
+	} else {
+		url = fmt.Sprintf("/market/aggregate/%s?interval=%s&start=%s&end=%s", data.Pair, data.Interval, data.Start, data.End)
+	}
+	resp, err := c.get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var getAggregatedPriceResponse responses.GetAggregatedPriceResponse
+	err = json.NewDecoder(resp.Body).Decode(&getAggregatedPriceResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &getAggregatedPriceResponse, nil
+}
